326a966fa5ba341cc10f475c1cffa3fca5dc8f9e
Uladzislau Kachurka <Uladzislay_Kachurka@epam.com>
2022-04-17 13:58:13 +0300

    fix(lab3): add targets

--- GeneticAlgorithm/Targets.ts
+++ GeneticAlgorithm/Targets.ts
@@ -3,6 +3,33 @@ import {GeneticValue} from "./GeneticValue";
 export class Targets {
     public static getTargetEquationA(value: GeneticValue): number {
         const [u, w, x, y, z] = value.getValues();
-        return Math.abs(x*y*z + x*y*(z**2) + y + u*x*(y**2)*(z**2) + (w**2)*x*(y**2)*z + 50);
+        return Math.abs(
+            x * y * z
+            +//-------------
+            x * y * (z ** 2)
+            +//---------------
+            y
+            +//--------------
+            u*x*(y ** 2)*(z ** 2)
+            +//--------------
+            (w ** 2)*x*(y ** 2)*z
+            + 50
+            );
+    }
+
+    public static getTargetEquationB(value: GeneticValue): number {
+        const [u, w, x, y, z] = value.getValues();
+        return Math.abs(
+            u*(w ** 2)*x*y
+            +//-------------
+            (u ** 2)*w*(x ** 2)*(y ** 2)*z
+            +//---------------
+            (u ** 2)*(w ** 2)*(x ** 2)*(z ** 2)
+            +//--------------
+            y
+            +//--------------
+            (w ** 2)*x*(y ** 2)*(z ** 2)
+            - 40
+        );
     }
 }
